650c00533113d539cbf4ff4281afd1bb02084597
beshketnyck <eelon.max@gmail.com>
2024-07-27 19:10:54 +0300

    Auto commit 07/27/2024 19:10:54

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from config import Config
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object('app.config.Config')
 
-    from app.main import main as main_blueprint
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
