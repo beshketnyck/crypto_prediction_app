fdee62d8eb6dcd6196c296bb781feb67ed587a0a
beshketnyck <eelon.max@gmail.com>
2024-07-28 01:14:18 +0300

    Auto commit 07/28/2024 01:14:18

--- app/data_analyser.py
+++ app/data_analyser.py
@@ -8,77 +8,70 @@ class DataAnalyser:
         self.base_api_url = base_api_url
 
     def fetch_data(self, start_date, end_date, coin_id):
-        start_datetime = datetime.combine(start_date, datetime.min.time()) + timedelta(days=1)
-        end_datetime = datetime.combine(end_date, datetime.min.time()) + timedelta(days=1)
-        
+        # Перевірка на часові межі, об'єднання дат для запиту
+        start_datetime = datetime.combine(start_date, datetime.min.time())
+        end_datetime = datetime.combine(end_date, datetime.max.time())
+
+        url = f'{self.base_api_url}/coins/{coin_id}/market_chart/range'
         params = {
             'vs_currency': 'usd',
             'from': int(start_datetime.timestamp()),
             'to': int(end_datetime.timestamp())
         }
         
-        url = f'{self.base_api_url}/coins/{coin_id}/market_chart/range'
         response = requests.get(url, params=params)
         data = response.json()
 
         if 'prices' not in data:
             raise ValueError("Invalid response from API: 'prices' key not found")
-
+        
         prices = data['prices']
         market_caps = data.get('market_caps', [])
         volumes = data.get('total_volumes', [])
 
-        df = pd.DataFrame(prices, columns=['timestamp', 'price'])
-        df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-        df.set_index('timestamp', inplace=True)
+        # Преобразування JSON-даних у DataFrame
+        df_prices = pd.DataFrame(prices, columns=['timestamp', 'price'])
+        df_prices['timestamp'] = pd.to_datetime(df_prices['timestamp'], unit='ms')
+        df_prices.set_index('timestamp', inplace=True)
 
         if market_caps:
-            df['market_cap'] = [mc[1] for mc in market_caps]
-        else:
-            df['market_cap'] = None
+            df_market_caps = pd.DataFrame(market_caps, columns=['timestamp', 'market_cap'])
+            df_market_caps['timestamp'] = pd.to_datetime(df_market_caps['timestamp'], unit='ms')
+            df_market_caps.set_index('timestamp', inplace=True)
+            df_prices['market_cap'] = df_market_caps['market_cap']
 
         if volumes:
-            df['volume'] = [vol[1] for vol in volumes]
-        else:
-            df['volume'] = None
+            df_volumes = pd.DataFrame(volumes, columns=['timestamp', 'volume'])
+            df_volumes['timestamp'] = pd.to_datetime(df_volumes['timestamp'], unit='ms')
+            df_volumes.set_index('timestamp', inplace=True)
+            df_prices['volume'] = df_volumes['volume']
 
-        df = df.resample('D').first()
-        
-        return df
+        # Вибірка даних на 12:00 кожного дня
+        daily_data = df_prices.resample('D').apply(lambda x: x.at_time('12:00') if '12:00' in x.index.time else x.iloc[0])
 
-    def fetch_social_data(self, coin_id):
-        url = f'{self.base_api_url}/coins/{coin_id}'
-        response = requests.get(url)
-        data = response.json()
+        # Переконайтесь, що індекс daily_data має тип datetime.date для порівняння з start_date і end_date
+        daily_data.index = daily_data.index.date
 
-        social_data = {
-            'twitter_followers': data.get('community_data', {}).get('twitter_followers', None),
-            'reddit_subscribers': data.get('community_data', {}).get('reddit_subscribers', None),
-            'facebook_likes': data.get('community_data', {}).get('facebook_likes', None)
-        }
-        
-        return social_data
+        # Фільтрація даних для точного діапазону дат
+        daily_data = daily_data[(daily_data.index >= start_date) & (daily_data.index <= end_date)]
 
-    def fetch_news(self, coin_id):
-        url = f'{self.base_api_url}/coins/{coin_id}/status_updates'
-        response = requests.get(url)
-        data = response.json()
-
-        if 'status_updates' not in data:
-            return []
-
-        news = [{'timestamp': item['created_at'], 'title': item['title'], 'description': item['description']} for item in data['status_updates']]
-        
-        return news
+        return daily_data
 
     def save_to_csv(self, df, filename):
+        # Збереження даних у CSV
         dir_path = './app/main/data'
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
+        
         filepath = os.path.join(dir_path, filename)
         df.to_csv(filepath)
         return filepath
 
-    def read_csv(self, filepath):
-        return pd.read_csv(filepath, parse_dates=True, index_col='timestamp')
+    def fetch_twitter_followers(self, coin_id):
+        url = f'{self.base_api_url}/coins/{coin_id}'
+        response = requests.get(url)
+        data = response.json()
 
+        if 'community_data' in data:
+            return data['community_data'].get('twitter_followers', 'N/A')
+        return 'N/A'
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, send_file, current_app
+from flask import Blueprint, render_template, request, current_app
 from wtforms import DateField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
@@ -10,15 +10,14 @@ main = Blueprint('main', __name__)
 class DataForm(FlaskForm):
     start_date = DateField('Start Date', format='%Y-%m-%d', validators=[DataRequired()])
     end_date = DateField('End Date', format='%Y-%m-%d', validators=[DataRequired()])
-    coin = SelectField('Cryptocurrency', choices=[('bitcoin', 'Bitcoin'), ('ethereum', 'Ethereum'), ('dogecoin', 'Dogecoin')], validators=[DataRequired()])
+    coin = SelectField('Cryptocurrency', choices=[('bitcoin', 'Bitcoin'), ('ethereum', 'Ethereum')])
     submit = SubmitField('Submit')
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = DataForm()
     data = None
-    social_data = None
-    news = None
+    twitter_followers = None
     if form.validate_on_submit():
         start_date = form.start_date.data
         end_date = form.end_date.data
@@ -28,12 +27,11 @@ def index():
         
         try:
             data = analyser.fetch_data(start_date, end_date, coin_id)
-            social_data = analyser.fetch_social_data(coin_id)
-            news = analyser.fetch_news(coin_id)
+            twitter_followers = analyser.fetch_twitter_followers(coin_id)
             
             filename = f'data_{coin_id}_{start_date}_{end_date}.csv'
             filepath = analyser.save_to_csv(data, filename)
         except ValueError as e:
             return render_template('index.html', form=form, error=f"Error: {str(e)}")
         
-    return render_template('index.html', form=form, data=data, social_data=social_data, news=news)
+    return render_template('index.html', form=form, data=data, twitter_followers=twitter_followers)
